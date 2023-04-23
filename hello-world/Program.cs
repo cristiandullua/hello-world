8bd25c94efcb11860eb9cf15eeaef6ab08111771
Cristian D Ullua <cristiandullua@byui.edu>
2023-04-23 02:54:09 -0300

    readme file update

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // Wait for a key press before exiting
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+}
